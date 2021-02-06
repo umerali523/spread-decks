8870e76d37e4b756fccfd2db125b1ddfa01cde39
Abar Ahmad <abrarahmad007@gmail.com>
2021-02-06 13:07:06 +0500

    project page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppLayoutComponent } from './layout/app-layout/app-layout.component';
 import { WebLayoutComponent } from './layout/web-layout/web-layout.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { MarketplaceComponent } from './pages/marketplace/marketplace.component';
 import { PortfolioValuationComponent } from './pages/portfolio-valuation/portfolio-valuation.component';
 import { ProjectValuationComponent } from './pages/project-valuation/project-valuation.component';
+import { TradeSharesComponent } from './pages/trade-shares/trade-shares.component';
 
 const routes: Routes = [
 
@@ -12,10 +14,16 @@ const routes: Routes = [
     { path:'' , component:HomepageComponent},
     { path:'home' , component:HomepageComponent},
     { path:'marketplace' , component:MarketplaceComponent},
+    { path:'traed-shares' , component:TradeSharesComponent},
+
+ 
+  ]},
+  {path:'' , component:AppLayoutComponent, children:[
     { path:'project-valuation' , component:ProjectValuationComponent},
     { path:'portfolio-valuation' , component:PortfolioValuationComponent},
-    { path:'**' , redirectTo:'home' , pathMatch:'full'}, 
   ]},
+  { path:'**' , redirectTo:'home' , pathMatch:'full'}, 
+
 ];
 
 @NgModule({
---
+++ src/app/pages/trade-shares/trade-shares.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trade-shares',
+  templateUrl: './trade-shares.component.html',
+  styleUrls: ['./trade-shares.component.scss']
+})
+export class TradeSharesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
