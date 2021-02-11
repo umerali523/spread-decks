9637a58319187ae5d3cb8efa5f7337e9e1b72dc9
Abar Ahmad <abrarahmad007@gmail.com>
2021-02-12 01:07:05 +0500

    secondary-owner.component

--- src/app/pages/secondary-owner/secondary-owner.component.ts
+++ src/app/pages/secondary-owner/secondary-owner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-secondary-owner',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SecondaryOwnerComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private render2: Renderer2,
+    private element: ElementRef
+
+  ) { }
 
   ngOnInit(): void {
   }
 
-}
+
+
+  
+  
+
+  }
+  
